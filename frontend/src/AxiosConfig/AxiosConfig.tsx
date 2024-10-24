acd843fe935b728905e8207b32d09c0a12dc4beb
AnilaBenny <135572616+AnilaBenny@users.noreply.github.com>
2024-10-24 13:16:07 +0530

    axioserror rectified

--- frontend/src/AxiosConfig/AxiosConfig.tsx
+++ frontend/src/AxiosConfig/AxiosConfig.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://persual.mooo.com" ,
   timeout: 100000,
   headers: {
     'Content-Type': 'application/json',
